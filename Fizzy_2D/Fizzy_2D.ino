ca2c263b3955fcc46088279e51789540ab35e95a
jlswbs <jlswbs@users.noreply.github.com>
2022-04-08 17:48:58 +0200

    Add files via upload

---
+++ Fizzy_2D/Fizzy_2D.ino
@@ -0,0 +1,90 @@
+// Fizzy 2D cellular automata //
+
+#include "hardware/structs/rosc.h"
+#include "st7735.h"
+
+#define WIDTH   40
+#define HEIGHT  80
+#define FULLW   80
+#define FULLH   160
+#define SCR     (WIDTH*HEIGHT)
+#define SCR2    (FULLW*FULLH)
+
+float randomf(float minf, float maxf) {return minf + (rand()%(1UL << 31))*(maxf - minf) / (1UL << 31);} 
+  
+  uint8_t col[2*SCR2];
+  
+  uint8_t calm = 233;
+  int CellIndex = 0;
+  float a = 4.7f;
+  float CellVal[SCR];
+    
+void rndrule(){
+
+  calm = 16 + rand()%233;
+  a = randomf(1.0f, 5.5f);
+  for (int k = 0; k < SCR; k++) CellVal[k] =  rand()%256;
+
+}
+
+static inline void seed_random_from_rosc(){
+  
+  uint32_t random = 0;
+  uint32_t random_bit;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int k = 0; k < 32; k++) {
+    while (1) {
+      random_bit = (*rnd_reg) & 1;
+      if (random_bit != ((*rnd_reg) & 1)) break;
+    }
+
+    random = (random << 1) | random_bit;
+  }
+
+  srand(random);
+}
+
+void setup() {
+  
+  seed_random_from_rosc();
+
+  ST7735_Init();
+  ST7735_FillScreen(ST7735_BLACK);
+
+  rndrule();
+  
+}
+
+void loop() {
+
+  for (int x = 0; x < WIDTH; x++) {
+      
+    for (int y = 0; y < HEIGHT; y++) {
+      
+      CellIndex = (CellIndex+1)%SCR;
+
+      uint8_t coll = a * CellVal[CellIndex];
+      uint16_t image = ST7735_COLOR565(coll, coll, coll);
+      col[((4*x)+(4*y)*FULLW)] = (uint8_t)(image >> 8) & 0xFF;
+      col[((4*x)+(4*y)*FULLW)+1] = (uint8_t)(image) & 0xFF;
+        
+      int below      = (CellIndex+1)%SCR;
+      int above      = (CellIndex+SCR-1)%SCR;
+      int left       = (CellIndex+SCR-HEIGHT)%SCR;
+      int right      = (CellIndex+HEIGHT)%SCR;
+      int aboveright = ((CellIndex-1) + HEIGHT + SCR)%SCR;
+      int aboveleft  = ((CellIndex-1) - HEIGHT + SCR)%SCR;
+      int belowright = ((CellIndex+1) + HEIGHT + SCR)%SCR;
+      int belowleft  = ((CellIndex+1) - HEIGHT + SCR)%SCR;
+
+      float NeighbourMix = powf((CellVal[left]*CellVal[right]*CellVal[above]*CellVal[below]*CellVal[belowleft]*CellVal[belowright]*CellVal[aboveleft]*CellVal[aboveright]), 0.125f);
+      CellVal[CellIndex] = fmod((((sqrtf(CellVal[CellIndex]))*(sqrtf(NeighbourMix)))+0.5f), calm); 
+   
+    }
+  
+  }
+
+  ST7735_DrawImage(0, 0, FULLW, FULLH, col);
+
+}
