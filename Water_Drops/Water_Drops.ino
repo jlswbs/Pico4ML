0920c064c6ab0ed02436f95430f31900b7435ab7
jlswbs <jlswbs@users.noreply.github.com>
2022-10-28 17:48:03 +0200

    Add files via upload

---
+++ Water_Drops/Water_Drops.ino
@@ -0,0 +1,83 @@
+// Water drops //
+
+#include "hardware/structs/rosc.h"
+#include "st7735.h"
+
+#define WIDTH   40
+#define HEIGHT  80
+#define FULLW   80
+#define FULLH   160
+#define SCR     (WIDTH*HEIGHT)
+#define SCR2    (FULLW*FULLH)
+#define DROPS   8
+
+  float randomf(float minf, float maxf) {return minf + (rand()%(1UL << 31))*(maxf - minf) / (1UL << 31);} 
+  
+  uint8_t col[2*SCR2];
+  
+  float p[WIDTH][HEIGHT];
+  float v[WIDTH][HEIGHT];
+    
+void rndrule(){
+
+  for (int y = 0; y < HEIGHT; y++){
+    for (int x = 0; x < WIDTH; x++){
+      p[x][y] = 0;
+      v[x][y] = 0;
+    }
+  }
+  
+  for (int i = 0; i < DROPS; i++) v[16+rand()%(WIDTH-32)][16+rand()%(HEIGHT-32)] = randomf(0.0f, TWO_PI);
+
+}
+
+  static inline void seed_random_from_rosc(){
+  
+  uint32_t random = 0;
+  uint32_t random_bit;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int k = 0; k < 32; k++) {
+    while (1) {
+      random_bit = (*rnd_reg) & 1;
+      if (random_bit != ((*rnd_reg) & 1)) break;
+    }
+
+    random = (random << 1) | random_bit;
+  }
+
+  srand(random);
+}
+
+void setup(){
+  
+  seed_random_from_rosc();
+
+  ST7735_Init();
+  ST7735_FillScreen(ST7735_BLACK);
+
+  rndrule();
+  
+}
+
+void loop(){
+
+  for (int y = 1; y < HEIGHT-1; y++){
+    for (int x = 1; x < WIDTH-1; x++){
+      v[x][y] += (p[x-1][y] + p[x+1][y] + p[x][y-1] + p[x][y+1]) * 0.25f - p[x][y];
+    }
+  }
+  
+  for (int y = 0; y < HEIGHT; y++){
+    for (int x = 0; x < WIDTH; x++){
+      p[x][y] += v[x][y];
+      uint8_t coll = 255.0f * fabs(constrain(p[x][y], -1.0f, 1.0f));
+      uint16_t image = ST7735_COLOR565(coll, coll, coll);
+      col[((4*x)+(4*y)*FULLW)] = (uint8_t)(image >> 8) & 0xFF;
+      col[((4*x)+(4*y)*FULLW)+1] = (uint8_t)(image) & 0xFF;
+    }
+  }
+  
+  ST7735_DrawImage(0, 0, FULLW, FULLH, col);
+
+}
