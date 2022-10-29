de817b14a3ec96d537c63c5f12523eaf5fa47a90
jlswbs <jlswbs@users.noreply.github.com>
2022-10-29 11:25:40 +0200

    Add files via upload

--- Chladni_Pattern/Chladni_Pattern.ino
+++ Chladni_Pattern/Chladni_Pattern.ino
@@ -10,20 +10,32 @@
 #define SCR     (WIDTH*HEIGHT)
 #define SCR2    (FULLW*FULLH)
 
+  float randomf(float minf, float maxf) {return minf + (rand()%(1UL << 31))*(maxf - minf) / (1UL << 31);} 
+  
   uint8_t col[2*SCR2];
-
+  
   float p[WIDTH][HEIGHT];
   float v[WIDTH][HEIGHT];
-  int ax, ay;
-
+  float a = 0.01f;
+  float b = 0.1f;
+  float c = 8.0f;
+    
 void rndrule(){
 
-  ax = rand()%WIDTH;
-  ay = rand()%HEIGHT;
+  a = randomf(0.005f, 0.099f);
+  b = randomf(0.049f, 0.199f);
+  c = randomf(1.0f, 12.0f);
+
+  for (int y = 0; y < HEIGHT; y++){
+    for (int x = 0; x < WIDTH; x++){
+      p[x][y] = 0;
+      v[x][y] = 0;
+    }
+  }
 
 }
 
-static inline void seed_random_from_rosc(){
+  static inline void seed_random_from_rosc(){
   
   uint32_t random = 0;
   uint32_t random_bit;
@@ -41,7 +53,7 @@ static inline void seed_random_from_rosc(){
   srand(random);
 }
 
-void setup() {
+void setup(){
   
   seed_random_from_rosc();
 
@@ -49,15 +61,15 @@ void setup() {
   ST7735_FillScreen(ST7735_BLACK);
 
   rndrule();
- 
+  
 }
 
 void loop(){
 
-  int frame = millis() / 40;
+  int frame = millis() / 32;
 
-  v[ax][ay] = 0;
-  p[ax][ay] = (sinf(frame * 0.01f) + sinf(frame * 0.1f)) * 8.0f;
+  v[WIDTH/2][HEIGHT/2] = 0;
+  p[WIDTH/2][HEIGHT/2] = (sinf(frame * a) + sinf(frame * b)) * c;
   
   for (int y = 1; y < HEIGHT-1; y++) {
     for (int x = 1; x < WIDTH-1; x++) {
@@ -65,16 +77,16 @@ void loop(){
     }
   }
   
-  for (int y = 1; y < HEIGHT-1; y++) {
-    for (int x = 1; x < WIDTH-1; x++) {
+  for (int y = 0; y < HEIGHT; y++){
+    for (int x = 0; x < WIDTH; x++){
       p[x][y] += v[x][y];
-      uint8_t coll = 128.0f * fabs(constrain(v[x][y], -1.0f, 1.0f));
+      uint8_t coll = 255.0f * fabs(constrain(v[x][y], -1.0f, 1.0f));
       uint16_t image = ST7735_COLOR565(coll, coll, coll);
       col[((4*x)+(4*y)*FULLW)] = (uint8_t)(image >> 8) & 0xFF;
       col[((4*x)+(4*y)*FULLW)+1] = (uint8_t)(image) & 0xFF;
     }
   }
-
+  
   ST7735_DrawImage(0, 0, FULLW, FULLH, col);
 
 }
